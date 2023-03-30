127bd388867a7d74c90e4956e4244e40c3edeeec
GICHEO <122686998+GICHEO@users.noreply.github.com>
2023-03-31 01:47:55 +0900

    Add Todo List Delete Function
    투두리스트 삭제 기능 구현

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,31 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
+const TODOS_KEY = "toDos";
 
-function paintToDo(newTodo) {
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+}
+
+function paintToDo(newToDoObj) {
   const li = document.createElement("li");
+  li.id = newToDoObj.id;
   const span = document.createElement("span");
+  span.innerText = newToDoObj.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
   li.appendChild(span);
-  span.innerText = newTodo;
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
@@ -14,7 +33,54 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  paintToDo(newTodo);
+  const newToDoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
+
+function sexyFilter() {}
+
+[1, 2, 3, 4].filter(sexyFilter);
+
+/*
+function sayHello(item) {
+  console.log("이제 준비~", item);
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  parsedToDos.forEach(sayHello);
+}
+이 코드와 
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  parsedToDos.forEach(item) =>console.log("이제 준비~", item);
+}
+
+이 코드는 근본적으로 같다. 동일한 것이다. 함수를 많이 작성하지 않고 싶으면
+후자의 기능을 쓰도록하자 더 짧게 쓰는 방법이기 때문이다. 보다시피 
+함수의 이름도 필요없고, funtion을 쓸 필요도 없다. 
+각각의아이템에 대해서 콘솔로그를 할것이라는 뜻. 
+이건 위 함수를 쓴것과 똑같다. 
+밑의 방법으로 작성하는 것을 arrow funtion 화살표함수라고 부른다. 
+우리는 에로우 함수를 사용할것이다. 
+ */
